4a168689e6da64f215e3e121bed5cb52f7656a04
Azhar Rafiq <51252461+azhar-rafiq@users.noreply.github.com>
2023-09-18 12:53:57 +0700

    Standard API added

--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
-import "fmt"
-func main(){
-	fmt.Println("Selamat datang di sistem Rating Input! ^-^)/")
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+type BaseResponse struct {
+	Status      bool        `json:"status"`
+	Message     string      `json:"message"`
+	TotalRating interface{} `json:"data"`
+}
+
+var totalInput int = 0
+var akumulasi int = 0
+
+// var teksKirimRating string = "Total Skor saat ini adalah: %akumulasi dari %totalInput input"
+
+func main() {
+	e := echo.New()
+
+	e.GET("/rating", GetRatingController)
+	e.GET("/rating/:nilai", GetAddRatingController)
+	e.Start(":8000")
+
+	fmt.Println("Selamat datang di sistem Rating Input! ^-^)/ ")
+}
+
+func GetRatingController(c echo.Context) error {
+
+	teksKirimRating := "Total Skor saat ini adalah: " + strconv.Itoa(akumulasi) + "dari " + strconv.Itoa(totalInput) + "input"
+
+	return c.JSON(http.StatusOK, BaseResponse{
+		Status:      true,
+		Message:     "Rating terbaru sedang loading ~",
+		TotalRating: teksKirimRating,
+	})
+}
+
+func GetAddRatingController(c echo.Context) error {
+	totalInput++
+	nilai, _ := strconv.Atoi(c.Param("nilai"))
+
+	// akumulasiBaru := akumulasi + nilai
+
+	akumulasi += nilai
+
+	teksKirimRating := "Total Skor saat ini adalah: " + strconv.Itoa(akumulasi) + "dari " + strconv.Itoa(totalInput) + "input"
+
+	return c.JSON(http.StatusOK, BaseResponse{
+		Status:      true,
+		Message:     "Rating terbaru sedang loading ~",
+		TotalRating: teksKirimRating,
+	})
 }
